befe3cc11c70b79911be7bc4549761fe59344e18
maruthan4r <mtcreatives@hotmail.com>
2023-05-21 09:27:58 +0200

    integrate search bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,28 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
+  faCopy,
   faFilter,
+  faGlobe,
   faQuestion,
   faRandom,
   faSearch,
 } from "@fortawesome/free-solid-svg-icons";
 import { useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import CircularProgress from "@mui/material/CircularProgress";
 import data from "../assets/data.json";
 
 export default function Home() {
+  const [allItems, setAllItems] = useState<
+    {
+      name: string;
+      description: string;
+      tags: string[];
+      image_url: string;
+      project_url: string;
+    }[]
+  >([]);
+
   const [items, setItems] = useState<
     {
       name: string;
@@ -18,8 +32,35 @@ export default function Home() {
       project_url: string;
     }[]
   >([]);
+  const [loaded, setLoaded] = useState(false);
+
+  const [id, setId] = useState(0);
+  const [showDetails, setShowDetails] = useState(false);
+
+  const getItems = async () => {
+    const url =
+      "https://raw.githubusercontent.com/emptea-xyz/data/main/emptea-catalog/items.json";
+    const response = fetch(url.toString())
+      .then((Response) => Response.json())
+      .then((result) => {
+        setAllItems(data);
+        setItems(data);
+      })
+      .then(() => setLoaded(true));
+  };
+
+  const searchByName = (tag: string) => {
+    const filtered = [];
+    for (let i = 0; i < allItems.length; i++) {
+      if (allItems[i].name.toLowerCase().includes(tag.toLowerCase())) {
+        filtered.push(allItems[i]);
+      }
+    }
+    setItems(filtered);
+  };
+
   useEffect(() => {
-    setItems(data);
+    getItems();
   }, []);
   return (
     <div className="home">
@@ -32,7 +73,12 @@ export default function Home() {
           <button className="filter-button">
             <FontAwesomeIcon icon={faFilter} />
           </button>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={(e) => {
+              searchByName(e.target.value);
+            }}
+          />
           <button className="search-button">
             <FontAwesomeIcon icon={faSearch} />
           </button>
@@ -40,32 +86,54 @@ export default function Home() {
       </div>
       <div className="blender-top"></div>
       <div className="items">
-        <div className="items-grid">
-          {items.map((i) => (
-            <div className="item">
-              <div className="item-content">
-                <div className="item-image">
-                  <img src={i.image_url} alt={i.name} />
-                </div>
-                <div className="item-name-box">
-                  <div className="item-name">
-                    {i.name.length > 9
-                      ? i.name[0] +
-                        i.name[1] +
-                        i.name[2] +
-                        i.name[3] +
-                        i.name[4] +
-                        i.name[5] +
-                        i.name[6] +
-                        i.name[7] +
-                        "..."
-                      : i.name}
+        {loaded && (
+          <div className="items-grid">
+            {items.map((i, index) => (
+              <motion.div
+                className="item"
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 0.2, delay: 0.2 }}
+                key={index}
+              >
+                <div className="item-content">
+                  <div className="item-image">
+                    <button
+                      className="item-image-button"
+                      onClick={() => {
+                        setShowDetails(true);
+                        setId(index);
+                      }}
+                    >
+                      <img src={i.image_url} alt={i.name} />
+                    </button>
+                  </div>
+
+                  <div className="item-name-box">
+                    <div className="item-name">
+                      {i.name.length > 9
+                        ? i.name[0] +
+                          i.name[1] +
+                          i.name[2] +
+                          i.name[3] +
+                          i.name[4] +
+                          i.name[5] +
+                          i.name[6] +
+                          i.name[7] +
+                          "..."
+                        : i.name}
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          ))}
-        </div>
+              </motion.div>
+            ))}
+          </div>
+        )}
+        {!loaded && (
+          <div className="spinner">
+            <CircularProgress color={"primary"} />
+          </div>
+        )}
       </div>
       <div className="blender-bottom"></div>
       <div className="socials">
@@ -76,6 +144,57 @@ export default function Home() {
           <FontAwesomeIcon icon={faQuestion} />
         </button>
       </div>
+      <AnimatePresence>
+        {showDetails && (
+          <motion.div
+            className="details"
+            onClick={() => setShowDetails(false)}
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2, delay: 0.2 }}
+          >
+            <motion.div
+              className="details-panel"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <div className="details-panel-content">
+                <div className="details-panel-image">
+                  <img src={items[id].image_url} alt={items[id].name} />
+                </div>
+                <div className="details-panel-name">{items[id].name}</div>
+                <div className="details-panel-description">
+                  {items[id].description}
+                </div>
+                <motion.div
+                  className="details-panel-link"
+                  onClick={() => {
+                    const text = document.getElementById("dpl-text");
+
+                    text!.innerHTML = "copied!";
+                    setTimeout(() => {
+                      text!.innerHTML = "copy website";
+                    }, 3000);
+                    navigator.clipboard.writeText(items[id].project_url);
+                  }}
+                >
+                  <FontAwesomeIcon icon={faCopy} />
+                  <div className="details-panel-link-text" id="dpl-text">
+                    copy website
+                  </div>
+                </motion.div>
+                <div className="details-panel-tags">
+                  {items[id].tags.map((tag, index) => (
+                    <div className="details-panel-tag" key={index}>
+                      {tag}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
